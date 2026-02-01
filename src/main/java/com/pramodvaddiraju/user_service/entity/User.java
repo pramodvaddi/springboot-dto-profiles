11c4d0548cdb5bcb3d196a6f01b4638ce571e9d2
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-01 16:13:12 +0530

    Added entity with minimal fields

---
+++ src/main/java/com/pramodvaddiraju/user_service/entity/User.java
@@ -0,0 +1,62 @@
+package com.pramodvaddiraju.user_service.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Username must be entered")
+    private String userName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Password must be entered")
+    private String password;
+
+    @NotBlank(message = "Department must be given")
+    private String department;
+
+
+    public User(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
+
